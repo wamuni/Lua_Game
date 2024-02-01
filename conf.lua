8c57d69a04ecff25634e7798ac1f0a38eda40371
wamuni <boshi.pan@outlook.com>
2024-02-01 23:54:25 +0000

    set icon, height, width, resizable or not

--- conf.lua
+++ conf.lua
@@ -7,4 +7,8 @@ function love.conf(t)
     t.gammacorrect = true     -- if system supports, it will use gamma to render it
     t.audio.mic = true        -- use mic on android
     t.window.title = "Cool Ball Game"
+    t.window.icon = "icon/window_icon.png"
+    t.window.width = 400       -- set windows width
+    t.window.height = 200      -- set windows height
+    t.window.resizable = false -- can't change size of window
 end
