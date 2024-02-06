9032cd6ed9afdbf6fcdef5b182cc42d13ceb0413
wamuni <boshi.pan@outlook.com>
2024-02-06 19:28:31 +0000

    make some changes

--- conf.lua
+++ conf.lua
@@ -1,14 +1,14 @@
 -- t contains all information
 function love.conf(t)
-    t.identity = "data/saves" -- save directory
+    t.identity = "data/saves"              -- save directory
     t.version = "1.0.0"
-    t.console = false         -- only for windows
-    t.externalstorage = true  -- save files for android only
-    t.gammacorrect = true     -- if system supports, it will use gamma to render it
-    t.audio.mic = true        -- use mic on android
+    t.console = false                      -- only for windows
+    t.externalstorage = true               -- save files for android only
+    t.gammacorrect = true                  -- if system supports, it will use gamma to render it
+    t.audio.mic = true                     -- use mic on android
     t.window.title = "Cool Ball Game"
-    t.window.icon = "icon/window_icon.png"
-    t.window.width = 400       -- set windows width
-    t.window.height = 200      -- set windows height
-    t.window.resizable = false -- can't change size of window
+    t.window.icon = "icon/window_icon.png" -- icon dones't work on mac
+    t.window.width = 400                   -- set windows width
+    t.window.height = 200                  -- set windows height
+    t.window.resizable = false             -- can't change size of window
 end
